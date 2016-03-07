06549502db2b66e98c5453619b133dfa38b210cb
Steve Harms <steven.harms@gsa.gov>
2016-03-07 10:52:25 -0500

    Remove unused configs

--- config/include/expire.clj
+++
@@ -1,2 +0,0 @@
-;; Expire old events from the index
-(periodically-expire 5 {:keep-keys [:host :service :tags :description :metric]})
--- config/include/logging.clj
+++
@@ -1,3 +0,0 @@
-;; ########### Configurations #############
-;; Log to console which will be redirected to riemann.stdout.log
-(logging/init {:console true})
