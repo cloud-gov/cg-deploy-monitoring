55569140eb7c5aa35d41e7f95fbcbc50fded98fd
James C Scott III <jcscottiii@users.noreply.github.com>
2016-03-15 13:45:24 -0400

    Update the url in clone-dash.sh

--- clone-dash.sh
+++ clone-dash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-URL='https://grafana.cloud.gov/api/search?query=&starred=false'
+URL='https://metrics.cloud.gov/api/search?query=&starred=false'
 for dashboard in `curl -u "${CREDENTIALS}" ${URL} | jq -r '.[].uri'`; do
 	filename=`echo $dashboard | sed 's/^db\///g'`
-	curl -u "${CREDENTIALS}" -o ${filename}.json https://grafana.cloud.gov/api/dashboards/${dashboard}
+	curl -u "${CREDENTIALS}" -o ${filename}.json https://metrics.cloud.gov/api/dashboards/${dashboard}
 done
