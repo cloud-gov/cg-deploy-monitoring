690e086b88184806ba6ea419fb737dd615544cf5
Mike Jacobi <michael.jacobi@altoros.com>
2016-08-03 12:50:05 -0700

    Set id to null and specify datasource name

--- clone-dash.sh
+++ clone-dash.sh
@@ -2,5 +2,6 @@
 URL='https://metrics.cloud.gov/api/search?query=&starred=false'
 for dashboard in `curl -u "${CREDENTIALS}" ${URL} | jq -r '.[].uri'`; do
 	filename=`echo $dashboard | sed 's/^db\///g'`
-	curl -u "${CREDENTIALS}" -o ${filename}.json https://metrics.cloud.gov/api/dashboards/${dashboard}
+	curl -u "${CREDENTIALS}" -o ${filename}.json https://metrics.fr.cloud.gov/api/dashboards/${dashboard}
 done
+# curl -vu "grafana:icmrhNHcoVEKBOrd+paptzhv4h4D6n9o8p/xRW6NgBHvC23DO7zFCC2S1hs2wnpq" "https://metrics.fr.cloud.gov/api/search?query=&starred=false"
