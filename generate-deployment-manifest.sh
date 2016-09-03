00ca5d00d7810207cd14f295a77dac777c8cf17b
Joshua Carp <jm.carp@gmail.com>
2016-09-03 13:07:27 -0400

    Use upstream grafana release.

---
+++ generate-deployment-manifest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -e
+
+which spruce > /dev/null 2>&1 || {
+  echo "Aborted. Please install spruce by following https://github.com/geofffranks/spruce#installation" 1>&2
+  exit 1
+}
+
+path="$(dirname $0)"
+
+SPRUCE_FILE_BASE_PATH=$path spruce merge "$path/monitoring.yml" "$@"
