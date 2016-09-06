8b9afe6f7cc30a5964d35d92f463f5b11df1e3f4
Joshua Carp <jm.carp@gmail.com>
2016-09-06 11:30:27 -0400

    Add kubernetes dashboards from heapster.

--- generate-deployment-manifest.sh
+++ generate-deployment-manifest.sh
@@ -9,4 +9,9 @@ which spruce > /dev/null 2>&1 || {
 
 path="$(dirname $0)"
 
+# Extract heapster dashboards
+mkdir $path/dashboards-tmp
+cat $path/src/heapster/grafana/dashboards/cluster.json | jq .dashboard > $path/dashboards-tmp/cluster.json
+cat $path/src/heapster/grafana/dashboards/pods.json | jq .dashboard > $path/dashboards-tmp/pods.json
+
 SPRUCE_FILE_BASE_PATH=$path spruce merge "$path/monitoring.yml" "$@"
