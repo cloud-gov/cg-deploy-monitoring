d4aed9acc36b306e9efaffb8dc7935e771e98933
Joshua Carp <jm.carp@gmail.com>
2017-09-01 16:11:35 -0400

    Replace or drop $(()) arithmetic to handle bosh regression.

--- cronjobs/check_awslogs.sh
+++ cronjobs/check_awslogs.sh
@@ -8,7 +8,7 @@ AWSCLI_PATH=/var/vcap/packages/awslogs/bin/aws
 
 GLOBAL_LAST_UPDATE=0
 
-for GROUP in  `$AWSCLI_PATH logs describe-log-groups | $JQ_PATH -r .logGroups[].logGroupName`; do
+for GROUP in `$AWSCLI_PATH logs describe-log-groups | $JQ_PATH -r .logGroups[].logGroupName`; do
     LAST_UPDATE=$($AWSCLI_PATH logs describe-log-streams --log-group-name=$GROUP --order-by LastEventTime --descending --max-items 1 | $JQ_PATH .logStreams[].lastEventTimestamp)
     if [ -z "$LAST_UPDATE" ]; then
         LAST_UPDATE=0
@@ -26,11 +26,11 @@ done
 
 # Emit a metric for each entry in our heatbeat group, where host = logStreamName, and metric = seconds since last update
 IFS=$'\n'
-for streaminfo in $(${AWSCLI_PATH} logs describe-log-streams --output text --max-items 1000 --order-by LastEventTime --descending --log-group-name=$HEARTBEAT_GROUP --query "logStreams[?lastEventTimestamp > \`$(($(($(date +%s) - 14400)) * 1000))\`][logStreamName, lastEventTimestamp]" | grep -v None); do
+for streaminfo in $(${AWSCLI_PATH} logs describe-log-streams --output text --max-items 1000 --order-by LastEventTime --descending --log-group-name=$HEARTBEAT_GROUP --query "logStreams[?lastEventTimestamp > \`$(expr '(' $(date +%s) - 14400 ')' '*' 1000 ))\`][logStreamName, lastEventTimestamp]" | grep -v None); do
     aws_id=$(echo ${streaminfo} | cut -f1)
-    last=$(( $(echo ${streaminfo} | cut -f2) / 1000 ))
+    last=$( expr $(echo ${streaminfo} | cut -f2) / 1000 )
 
-    ${RIEMANNC_PATH} --service "aws.logs.describe-log-streams.heartbeat" --host ${aws_id} --ttl ${TTL} --metric_sint64 $(( $(date +%s) - ${last} ))
+    ${RIEMANNC_PATH} --service "aws.logs.describe-log-streams.heartbeat" --host ${aws_id} --ttl ${TTL} --metric_sint64 $(expr $(date +%s) - ${last} )
 done
 
 
